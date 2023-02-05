cc8eb7023b4b53b2c1548b8c85d18df651ca7734
Xitiz Magar <xitiz143@gmail.com>
2023-02-05 21:01:34 +0545

    addded-new-h1

--- cal.js
+++ cal.js
@@ -3,7 +3,7 @@ let buttons = document.querySelectorAll("button")
 Array.from(buttons).forEach((button)=>{
     button.addEventListener("click",((e)=>{
         console.log(e.target)
-        if(e.target.innerHTML == "="){
+        if(e.target.innerHTML == "="){ 
             string = eval(string);
             document.querySelector("input").value = string;
         }
